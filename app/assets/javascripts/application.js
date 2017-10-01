9b8661445bd8958f7ff18778de5597099a335d39
queztalsa <quetzalsaez@gmail.com>
2017-10-01 19:01:38 -0300

    css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,7 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
-//= require jquery
-//= require bootstrap-sprockets
+
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
