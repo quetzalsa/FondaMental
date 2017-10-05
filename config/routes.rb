1db0b654cce0f5ebe940ec2d3fde262510b14c3d
queztalsa <quetzalsaez@gmail.com>
2017-10-05 15:12:19 -0300

    media

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get "/fotos" => "fonda_mental#fotos"
+  get "/media" => "fonda_mental#media"
   get "/videos" => "fonda_mental#videos"
   get "/fonda_mental" => "fonda_mental#fonda_mental"
   root "player#player"
