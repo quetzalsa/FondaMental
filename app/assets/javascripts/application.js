21a28b20fc7d77d459f17d278d94e7a1582ddc12
queztalsa <quetzalsaez@gmail.com>
2017-10-06 03:29:05 -0300

    hbih

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery2
+//= require jquery_ujs
 //= require rails-ujs
 //= require turbolinks
 //= require lightbox
-//= require jquery2
-//= require jquery_ujs
 //= require_tree .
 
 
