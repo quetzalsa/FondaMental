528e7ffa7f2691544aa2c093aba6e87031c6974b
queztalsa <quetzalsaez@gmail.com>
2017-10-07 12:27:10 -0300

    final menu bien

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -47,20 +47,7 @@ function topFunction() {
     document.documentElement.scrollTop = 0; // For IE and Firefox
 } 
 
-var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].onclick = function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  }
-}
 
 
 
