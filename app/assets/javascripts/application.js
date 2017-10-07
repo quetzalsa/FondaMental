7abf6bde6280178f8551bd8e19f1bf6ad542d482
queztalsa <quetzalsaez@gmail.com>
2017-10-07 01:12:03 -0300

    scroll info

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,21 @@
 //= require_tree .
 
 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
+}
+ 
 
 function myFunction() {
     var x = document.getElementById("myTopnav");
