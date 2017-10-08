fa067776ae1adaf691b2befade0e239c481c61cb
queztalsa <quetzalsaez@gmail.com>
2017-10-07 23:01:17 -0300

    turbo prueba

--- app/assets/javascripts/scrolltop.js
+++ app/assets/javascripts/scrolltop.js
@@ -1,18 +1,20 @@
+
+
 document.addEventListener("turbolinks:load", function() {
     $(window).scroll(function() {
-        var height = $(window).scrollTop();
-        if (height > 100) {
-            $('#myBtn').fadeIn();
-        } else {
-            $('#myBtn').fadeOut();
-        }
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#myBtn').fadeIn();
+    } else {
+        $('#myBtn').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#myBtn").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
     });
-    $(document).ready(function() {
-        $("#myBtn").click(function(event) {
-            event.preventDefault();
-            $("html, body").animate({ scrollTop: 0 }, "slow");
-            return false;
-        });
-    
-    });
+
+});
   })
