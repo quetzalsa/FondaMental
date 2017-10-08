a51b74f5805d9072731a2bcbeb9a1e621a211efb
queztalsa <quetzalsaez@gmail.com>
2017-10-07 22:34:02 -0300

    scrolltopnuevo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,23 +30,6 @@ function myFunction() {
     }
 } 
 
-$(window).scroll(function() {
-    var height = $(window).scrollTop();
-    if (height > 100) {
-        $('#myBtn').fadeIn();
-    } else {
-        $('#myBtn').fadeOut();
-    }
-});
-$(document).ready(function() {
-    $("#myBtn").click(function(event) {
-        event.preventDefault();
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-        return false;
-    });
-
-});
-
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
