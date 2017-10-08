b5b809bb01f1322c53a8a0e8b5dcb53b216b4c61
queztalsa <quetzalsaez@gmail.com>
2017-10-07 22:23:34 -0300

    scrolling top

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,28 +30,22 @@ function myFunction() {
     }
 } 
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#myBtn').fadeIn();
     } else {
-        document.getElementById("myBtn").style.display = "none";
+        $('#myBtn').fadeOut();
     }
-}
+});
+$(document).ready(function() {
+    $("#myBtn").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Chrome, Safari and Opera
-    document.documentElement.scrollTop = 0; // For IE and Firefox
-} 
-
-
-
-
-
-
+});
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
