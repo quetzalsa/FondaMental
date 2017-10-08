7486157cc7e57465252ede2cebb45c3ec712b9f6
queztalsa <quetzalsaez@gmail.com>
2017-10-07 22:51:52 -0300

    scroll load

--- app/assets/javascripts/scrolltop.js
+++ app/assets/javascripts/scrolltop.js
@@ -1,16 +1,18 @@
-$(window).scroll(function() {
-    var height = $(window).scrollTop();
-    if (height > 100) {
-        $('#myBtn').fadeIn();
-    } else {
-        $('#myBtn').fadeOut();
-    }
-});
-$(document).ready(function() {
-    $("#myBtn").click(function(event) {
-        event.preventDefault();
-        $("html, body").animate({ scrollTop: 0 }, "slow");
-        return false;
+document.addEventListener("turbolinks:load", function() {
+    $(window).scroll(function() {
+        var height = $(window).scrollTop();
+        if (height > 100) {
+            $('#myBtn').fadeIn();
+        } else {
+            $('#myBtn').fadeOut();
+        }
     });
-
-});
+    $(document).ready(function() {
+        $("#myBtn").click(function(event) {
+            event.preventDefault();
+            $("html, body").animate({ scrollTop: 0 }, "slow");
+            return false;
+        });
+    
+    });
+  })
