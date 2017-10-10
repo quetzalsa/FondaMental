e786031c7040905b86d104d0254ed4a5241c3e8b
queztalsa <quetzalsaez@gmail.com>
2017-10-10 20:50:11 -0300

    i

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -101,7 +101,7 @@ document.addEventListener("turbolinks:load", function() {
          
         new ScrollMagic.Scene({
                 triggerElement: "#trig1",
-                offset: 300
+                offset: 200
             })
             .setTween(fromBottomTimeline)
             .duration(400)
@@ -124,7 +124,7 @@ document.addEventListener("turbolinks:load", function() {
          
         new ScrollMagic.Scene({
                 triggerElement: "#trig3",
-                offset: 200
+                offset: 300
             })
             .setTween(fromBottomTimeline)
             .duration(400)
