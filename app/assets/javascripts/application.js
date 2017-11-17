303e9fcd35bd5e05683c740f2a056abfcd2a7b53
queztalsa <quetzalsaez@gmail.com>
2017-11-17 15:30:47 -0300

    cambios finales

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -88,11 +88,12 @@ document.addEventListener("turbolinks:load", function() {
 
     var acc = document.getElementsByClassName("accordion");
     var i;
-
+    
     for (i = 0; i < acc.length; i++) {
       acc[i].onclick = function() {
         this.classList.toggle("active");
         var panel = this.nextElementSibling;
+        
         if (panel.style.maxHeight){
           panel.style.maxHeight = null;
         } else {
